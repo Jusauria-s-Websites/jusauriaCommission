aa309cad594f0ca4af0e4f7d831570dd40ede7d8
Juleysi <juleysi.deutschmann@htlpinkafeld.at>
2024-01-30 12:45:30 +0100

    Terms and Services version 1

--- jusauria-commission/src/app/Screen/terms/terms.component.ts
+++ jusauria-commission/src/app/Screen/terms/terms.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavbarComponent } from '../../ReuseComponents/navbar/navbar.component';
+import { FooterComponent } from '../../ReuseComponents/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { DropdownComponent } from '../../ReuseComponents/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,DropdownComponent, NavbarComponent, FooterComponent],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.css'
 })
 export class TermsComponent {
-
+ 
 }
