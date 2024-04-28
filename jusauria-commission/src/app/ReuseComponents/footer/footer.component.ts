ce2f5000d6b1c9b8370801db4ed01dae8b0022ad
Jusauria <mewtumew1@gmail.com>
2024-04-28 22:55:39 +0200

    Footer Router Fix to About page

--- jusauria-commission/src/app/ReuseComponents/footer/footer.component.ts
+++ jusauria-commission/src/app/ReuseComponents/footer/footer.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { SocialServiceService } from '../../Services/SocialService/SocialService.service';
 import { Social } from '../../Pojos/Objects/Social';
 import { CommonModule } from '@angular/common';
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
