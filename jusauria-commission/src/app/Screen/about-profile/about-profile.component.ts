c221641a7b4aedfd2138b3d420d4f7e55c38e2ab
Jusauria <mewtumew1@gmail.com>
2024-04-28 23:03:05 +0200

    About picture setting for web.

--- jusauria-commission/src/app/Screen/about-profile/about-profile.component.ts
+++ jusauria-commission/src/app/Screen/about-profile/about-profile.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../ReuseComponents/navbar/navbar.component';
 import { FooterComponent } from '../../ReuseComponents/footer/footer.component';
+import { Albums } from '../../Pojos/enums/Albums.enum';
+import { ImgurAPIService } from '../../Services/ImgurService/imgurAPI.service';
 
 @Component({
   selector: 'app-about-profile',
@@ -12,11 +14,18 @@ import { FooterComponent } from '../../ReuseComponents/footer/footer.component';
 })
 export class AboutProfileComponent{
   age: number;
-  constructor(){
+  picture: string | undefined;
+  constructor(private imgurService: ImgurAPIService){
+    //age update
     if(new Date().getMonth()<3){
       this.age=new Date().getFullYear()-2006;
     }else{
       this.age=new Date().getFullYear()-2005;
     }
+
+    //picture inside the serviceIcons
+    imgurService.getAlbumImages(Albums.ServiceIconAlbum).then(item=>{
+      this.picture=item.at(item.length-1)?.link;
+    })
   }
 }
