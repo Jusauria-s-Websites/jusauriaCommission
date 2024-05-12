c48d3877457ce733a0142b2a050c58f1b877bc38
Jusauria <mewtumew1@gmail.com>
2024-05-12 08:40:51 +0200

    Adding the LocalStoreService

---
+++ jusauria-commission/src/app/Services/LocalStoreService/local-store-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Albums } from '../../Pojos/enums/Albums.enum';
+import { AlbumImage } from '../../Pojos/Objects/AlbumImage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStoreServiceService {
+
+  constructor() { }
+
+  addToStore(folderName: Albums, values:AlbumImage[]):Promise<void>{
+    return new Promise((resolve)=>{
+      localStorage.setItem(folderName, JSON.stringify(values));
+      resolve();
+    })
+    
+  }
+  getFromStore(folderName:Albums): Promise<AlbumImage[]>{
+    return new Promise((resolve,reject)=>{
+      let array =localStorage.getItem(folderName)
+
+      if(array){
+        console.log("Access from local")
+        resolve(JSON.parse(array));
+      } 
+      else reject();
+    })
+    
+  }
+  deleteFromStore(folderName:Albums): Promise<void>{
+    return new Promise((resolve)=>{
+      localStorage.removeItem(folderName);
+      resolve();
+    })
+  }
+}
