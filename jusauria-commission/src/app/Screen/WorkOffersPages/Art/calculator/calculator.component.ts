65e6c3dc796f8739a3e4bcb8150e2ec6fa6f94d4
Jusauria <mewtumew1@gmail.com>
2024-05-14 15:14:25 +0200

    Calculator finishes and adjusting. Done Calculator

--- jusauria-commission/src/app/Screen/WorkOffersPages/Art/calculator/calculator.component.ts
+++ jusauria-commission/src/app/Screen/WorkOffersPages/Art/calculator/calculator.component.ts
@@ -21,11 +21,16 @@ export class CalculatorComponent {
   typeOfComm:string = "";
   specificComm:number=0;
   nsfw=false;
-  
-  
+  // -same-
   charSheet:boolean[]=[false,false,false];
   custBaseAnimal:number=1;
   typeBasicNames:string[]=[];
+  //-mix-
+  characterArray:{
+    type:string,
+    nsfw:boolean,
+  }[]=[]
+  lineCol:boolean=false;
   
   constructor(private render: Renderer2){
     this.typeBasic= OfferServiceService.basicArtComms;
@@ -51,7 +56,7 @@ export class CalculatorComponent {
       this.render.removeClass(document.getElementById("calculator"),"invisible")
       this.render.addClass(document.getElementById("calculator"),"goUp")
       this.render.removeClass(document.getElementById("calculator"),"goDown")
-    },1600)
+    },1500)
   }
   EndAnimation(){
     this.render.removeClass(document.getElementById("calculator"),"goUp")
@@ -67,7 +72,7 @@ export class CalculatorComponent {
             this.render.removeClass(document.getElementById("calculatorIcon"),"dropDown")
             this.render.addClass(document.getElementById("calculator"),"invisible")
             this.render.addClass(document.getElementById("calculatorIcon"),"dropUp")
-          },200)
+          },250)
           clearInterval(checkPosition)
         }
       },10)
@@ -75,9 +80,9 @@ export class CalculatorComponent {
     
   }
   calc(){
-    if(this.amountChar<1) this.amountChar=1
     this.moneyAmount=0;
     if(this.calcType){
+      if(this.amountChar<1) this.amountChar=1
       if(this.typeBasicNames.includes(this.typeOfComm)){
         this.moneyAmount= this.typeBasic.find(e=>e.type==this.typeOfComm)?.prices[this.specificComm][1];
         this.moneyAmount *= (this.amountChar > 1 ? 1+this.amountChar * 0.5 : 1);
@@ -91,12 +96,52 @@ export class CalculatorComponent {
         this.moneyAmount *=(this.nsfw?1.2:1);
       }else{
         this.moneyAmount= this.special.find(e=>e.type==this.typeOfComm)?.prices[this.specificComm][1];
-        this.moneyAmount+= 1+this.custBaseAnimal;
+        this.moneyAmount+= 1+ Number(this.custBaseAnimal);
         this.moneyAmount *=(this.nsfw?1.2:1);
-        
       }
     }else {
-      
+      if(this.specificComm<2)this.specificComm=3
+      //sort them
+      this.characterArray.sort((a,b)=>a.type.localeCompare(b.type));
+      let currentName = this.characterArray[0].type;
+      let typeHelp=false;
+      for(let i=0;i<this.characterArray.length;i++){
+        let moneyHelp= this.typeBasic.find(e=>e.type==this.characterArray[i].type)?.prices[this.specificComm][1];
+        console.log(moneyHelp);
+        moneyHelp +=(this.lineCol?2:0);
+        console.log(moneyHelp);
+        moneyHelp *=(this.characterArray[i].nsfw?1.2:1);
+        console.log(moneyHelp);
+        if(currentName==this.characterArray[i].type && typeHelp){
+          moneyHelp/=2;
+          console.log("Halfed")
+        } 
+        else if(currentName!=this.characterArray[i].type){
+          console.log("new type")
+          currentName = this.characterArray[i].type
+          typeHelp = true;
+        } 
+        else {
+          console.log("nex half")
+          typeHelp=true;
+        }
+        console.log(moneyHelp);
+        this.moneyAmount+=moneyHelp;
+        console.log("-----\n"+this.moneyAmount);
+      }
+    }
+    this.moneyAmount=parseFloat(this.moneyAmount.toFixed(2))
+  }
+  forArray(){
+    if(this.amountChar<1) this.amountChar=1
+    if(this.characterArray.length<this.amountChar){
+      this.characterArray.push({type:"",nsfw:false});
+      return new Array(this.amountChar);
+    }else if(this.characterArray.length>this.amountChar){
+      this.characterArray.pop();
+      return new Array(this.amountChar);
+    }else{
+      return new Array(this.amountChar);
     }
   }
 }
